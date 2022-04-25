7afd772cba62e763d10f6675b07486125b5d0262
Karam Abbas <karam.abbas@digital.hmrc.gov.uk>
2022-04-25 16:11:25 +0100

    PSEC-1483 WIP Setup

---
+++ cmd/InspectorManager/main.go
@@ -0,0 +1,30 @@
+package main
+import ("flag" 
+"fmt" 
+"github.com/platsec-inspector-manager/inspectorInput")
+
+func main() {
+	awsAccount := flag.String("account", "", "AWS account")
+	username := flag.String("username", "", "AWS username")
+	region := flag.String("region", "eu-west-2", "AWS region")
+	profile := flag.String("profile", "", "AWS profile")
+	action := flag.String("action", "SUPRESS", "action to apply")
+	filterName := flag.String("filter-name", "", "filter name")
+	filterType := flag.String("filter-type", "cve", "type of filter")
+	comparissonOperator := flag.String("comparisson-opertaor", "EQUALS", "comaprsison operator")
+	mfaToken := flag.String("mfa-token", "", "MFA token")
+	flag.Parse()
+
+	myUserInput := inspectorInput.UserInput{
+		AwsAccount: *awsAccount,
+		Usernname: *username,
+		Region: *region,
+		Profile: *profile,
+		Action: *action,
+		FilterName: *filterName,
+		FilterType: *filterType,
+		ComparissionOperator: *comparissonOperator,
+		MfaToken: *mfaToken,
+	}
+	fmt.Printf("%s", myUserInput.AwsAccount)
+}
---
+++ inspectorInput/inspectorInput.go
@@ -0,0 +1,14 @@
+package inspectorInput
+
+type UserInput struct {
+	AwsAccount string
+	Usernname string
+	Region string
+	Profile string
+	Action string
+	FilterName string
+	FilterType string
+	ComparissionOperator string
+	MfaToken string
+}
+
